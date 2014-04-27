ebe035c2873bffddda04923863092dbe72960ba9
Serkan Serttop <serkanserttop@yahoo.com>
2014-04-27 10:52:18 +0300

    Final solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#Creates a scoped environment that returns 4 functions and enables caching for the matrix and its inverse
+makeCacheMatrix <- function(mat = matrix()) {
+  #inverse is not calculated in the initialization, hence it is set to NULL
+  inverse <- NULL
+  set <- function(newMat) {
+    mat <<- newMat
+    #invalidate cache of inverse upon setting new matrix
+    inverse <<- NULL
+  }
+  get <- function() mat
+  setInverse <- function(solved) inverse <<- solved
+  getInverse <- function() mat
+  #return 4 functions scoped in this environment
+  list(set = set, get = get,
+    setInverse = setInverse,
+    getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inverse <- mat$getInverse()
+  #we check if our inverse is in the cache
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    #if it is in the cache, just return it
+    return(inverse)
+  }
+  #else, we need to solve it
+  data <- mat$get()
+  inverse <- solve(data, ...)
+  #and then set the inverse cache with the new solution
+  mat$setInverse(inverse)
+  inverse
 }
